48334920d8f32e611d3dda0a3ce924898cd176a8
MohdSaif-1807 <saifmohd1807@gmail.com>
2022-11-16 11:30:01 +0530

    This is Internal 1 Exam

---
+++ src/main/java/in/kmit/ExamMaven/App.java
@@ -0,0 +1,13 @@
+package in.kmit.ExamMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE LAB Internal-I exam , The Created Project is Exam Maven" );
+    }
+}
